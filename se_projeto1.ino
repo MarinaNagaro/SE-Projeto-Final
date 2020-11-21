b7ad88aedb7e2c0283b8114e74b093e86f76dbfb
Marina N <60940114+MarinaNagaro@users.noreply.github.com>
2020-11-21 13:55:04 -0300

    .
    Identação do código

--- se_projeto1.ino
+++ se_projeto1.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-#define pino_trigger  7
+#define pino_trigger 7
 #define pino_echo 6
 #define pressao A0
 #define bomba 8
@@ -10,10 +10,10 @@ int agua;
 
 
 void setup() {
-  pinMode(bomba, OUTPUT);
-  pinMode(pressao, INPUT);
-  pinMode(pino_trigger, OUTPUT);
-  pinMode(pino_echo, INPUT);
+  pinMode(bomba, OUTPUT);//motor
+  pinMode(pressao, INPUT);//botao
+  pinMode(pino_trigger, OUTPUT);//sensor
+  pinMode(pino_echo, INPUT);//sensor
   
   Serial.begin(9600);
   lcd.begin(16, 2);
@@ -24,82 +24,57 @@ void setup() {
 void loop() {
   
   agua = distancia();
-  
   //quando o botão não esta pressionado
- // while (digitalRead(pressao) == LOW){
+  // while (digitalRead(pressao) == LOW){
   
-    digitalWrite(motor, 0);
-   lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("sem pressao");
-    
-    if (agua <= 100){
-      
-      
-      lcd.setCursor(0,1);
-      lcd.print("caixa cheia");
-      	
-    
-    }
-    
-    if (agua > 100 && agua <= 250 ){
-    
-      lcd.setCursor(0,1);
-      lcd.print("caixa na metade");
-      	
-    }
-    
-    if (agua >250){
-      
-      
-      lcd.setCursor(0,1);
-      lcd.print("caixa vazia");
-      	
-    
-    }
-    delay(500);
+  digitalWrite(motor, 0);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("sem pressao");
+  
+  if (agua <= 100){
+    lcd.setCursor(0,1);
+    lcd.print("caixa cheia");
+  }
+  if (agua > 100 && agua <= 250 ){
+    lcd.setCursor(0,1);
+    lcd.print("caixa na metade");    	
+  }  
+  if (agua >250){     
+    lcd.setCursor(0,1);
+    lcd.print("caixa vazia");    	
+  }
+  delay(500);
  // }
-  
- 
- 
-  // quando o botão esta pressionado
-  while (digitalRead(pressao) == HIGH){ 
-    
-     lcd.clear();
-    if (agua > 100 && agua < 300){
-    
-      lcd.setCursor(0,0);
-      lcd.print("enchendo a caixa");
-       lcd.setCursor(0,1);
-      lcd.print("motor ligado");
-      digitalWrite(motor, 36);
+
+ // quando o botão esta pressionado
+  while (digitalRead(pressao) == HIGH){
+   lcd.clear();
+   if (agua > 100 && agua < 300){
+     lcd.setCursor(0,0);
+     lcd.print("enchendo a caixa");
+     lcd.setCursor(0,1);
+     lcd.print("motor ligado");
+     digitalWrite(motor, HIGH);
     }
-    
     if (agua <= 100){
-    
-      lcd.setCursor(0,0);
-      lcd.print("caixa cheia");
-      lcd.setCursor(0,1);
-      lcd.print("motor desligado");
-      digitalWrite(motor, 0);
-      	
+     lcd.setCursor(0,0);
+     lcd.print("caixa cheia");
+     lcd.setCursor(0,1);
+     lcd.print("motor desligado");
+     digitalWrite(motor, LOW);	
     }
     delay(500);
   }
-  // fim dos testes
-  
-  
+  // fim dos testes 
 }
 
 //FUNÇÃO
 float distancia(){ 
-
   float dur;
   digitalWrite(pino_trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(pino_trigger, LOW);
-  dur = pulseIn(pino_echo, HIGH);
-  
-  return dur/58.82; 
-  
+  dur = pulseIn(pino_echo, HIGH);  
+  return dur/58.82;   
 } 
